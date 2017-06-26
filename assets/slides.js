3c58f3fda37bb59d582e5a2793a8c018d1c9fd65
loic <loic.bourg63@gmail.com>
2017-06-27 00:26:10 +0200

    wip

--- assets/slides.js
+++ assets/slides.js
@@ -1,7 +1,11 @@
 var slides = [
     require('slides/titre.html'),
     require('slides/presentation.html'),
-    require('slides/jwt-cest-quoi.html')
+    require('slides/jwt-cest-quoi.html'),
+    require('slides/a-quoi-ca-ressemble.html'),
+    require('slides/comment-ca-marche.html'),
+    require('slides/comment-on-lutilise.html'),
+    require('slides/interet.html'),
 ].join('\n');
 
 module.exports = slides;
