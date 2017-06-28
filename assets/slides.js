7dcc3ae9523dda6efea7c685e6cf4d3595871a7e
loic <loic.bourg63@gmail.com>
2017-06-28 02:51:35 +0200

    goto: sleep

--- assets/slides.js
+++ assets/slides.js
@@ -1,11 +1,22 @@
 var slides = [
     require('slides/titre.html'),
     require('slides/presentation.html'),
+    require('slides/session-classique.html'),
+    require('slides/jose.html'),
     require('slides/jwt-cest-quoi.html'),
     require('slides/a-quoi-ca-ressemble.html'),
     require('slides/comment-ca-marche.html'),
+    require('slides/example-utilisation.html'),
+    require('slides/avantage.html'),
+    require('slides/inconvenient.html'),
+    require('slides/claims.html'),
+    require('slides/header-algorithme.html'),
     require('slides/comment-on-lutilise.html'),
-    require('slides/interet.html'),
+    require('slides/scaling-horizontal.html'),
+    require('slides/appel-service-tiers.html'),
+    require('slides/single-sign-on.html'),
+    require('slides/ressources-interessantes.html'),
+    require('slides/questions.html'),
 ].join('\n');
 
 module.exports = slides;
